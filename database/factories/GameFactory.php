7979aeed7681b690afa81497c1513bc65fd72023
Franko Vekić <franko.vekic@gmail.com>
2022-05-25 22:25:21 +0200

    Model, Factory, Migrations

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
+ */
+class GameFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->jobTitle(),
+            'quantity'=>10,
+            'price'=>$this->faker->randomFloat(2,1,100),
+            'description'=>$this->faker->paragraph(7)
+        ];
+    }
+}
