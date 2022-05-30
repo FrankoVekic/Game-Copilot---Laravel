a5a6b4c964f14475028a8c9da9bc141e31c6554e
Franko Vekić <franko.vekic@gmail.com>
2022-05-30 20:54:38 +0200

    Basic equipment view setup

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
     public function equipment()
     {
         return view('products.equipment',[
-            'equipment'=>Equipment::all()
+            'equipment'=>Equipment::latest()->simplePaginate(6)
         ]);
     }
 }
